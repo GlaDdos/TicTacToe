111dda3d5d011449b95d49d94539376ced5bd3be
kamil_kawa <kamil.kawa@gmail.com>
2016-03-15 21:35:23 +0100

    next iteration of not working

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ var boardState = {
   8: null,
   9: null,
 };
+
+var freeMoves = [];
+
 var winCombination = [
   [1,2,3],
   [4,5,6],
@@ -26,7 +29,8 @@ $(document).ready(function(){
     var cellId = this;
     player = playerClick(player, cellId);
     checkBoard(boardState, winCombination);
-    //console.log(boardState);
+    console.log(freeMoves);
+
   });
 });
 
@@ -40,14 +44,53 @@ function playerClick(player, cellId){
     boardState[$(cellId).attr('id')] = $(cellId).html();
   }
 
+  var a = compMove(boardState, winCombination);
+  console.log(a);
+
   return !player;
 }
 
 function checkBoard(boardState, winCombination){
+  freeMoves = [];
   for(var i = 0; i < winCombination.length; i++){
-    if((boardState[winCombination[i][0]] == boardState[winCombination[i][1]] == boardState[winCombination[i][2]]) && (boardState[winCombination[i][0]] !== null)){
+    if((boardState[winCombination[i][0]] == boardState[winCombination[i][1]]) && ( boardState[winCombination[i][1]] == boardState[winCombination[i][2]]) && (boardState[winCombination[i][0]] !== null)){
       console.log("WINWINWIN");
     }
-    console.log("i = " + i + ": " + boardState[winCombination[i][0]] && boardState[winCombination[i][1]] && boardState[winCombination[i][2]]);
   }
+
+  for(var i = 1; i <= 9; i++ ){
+    if(boardState[i] === null){
+      freeMoves.push(i);
+    }
+  }
+}
+
+
+
+function compMove(boardState, winCombination){
+  //1
+  for(var i=0; i < winCombination.length; i++){
+    if((boardState[winCombination[i][0]] == boardState[winCombination[i][1]]) && (boardState[winCombination[i][1]] !== null) ){
+      if( boardState[winCombination[i][2]] === null ){
+        return winCombination[i][2];
+      }
+    }else if ((boardState[winCombination[i][1]] == boardState[winCombination[i][2]]) && (boardState[winCombination[i][2]] !== null)) {
+      if(boardState[winCombination[i][0]] === null){
+        return winCombination[i][0];
+      }
+    }else if ((boardState[winCombination[i][0]] == boardState[winCombination[i][2]]) && (boardState[winCombination[i][2]] !== null)) {
+      if(boardState[winCombination[i][1]] === null){
+        return winCombination[i][1];
+      }
+    }
+  }
+
+  //2
+  if(boardState[5] === null){
+    return winCombination[1][1];
+  }
+
+  //3
+  return freeMoves[Math.floor((Math.random() * freeMoves.length))];
+
 }
